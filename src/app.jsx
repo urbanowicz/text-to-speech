0bb993ccddd596aa1c36c2fdf4820491b2608b1f
Tomasz Urbanowicz <tomasz.urbanowicz@tuta.io>
2018-09-29 16:59:25 +0200

    Convert speech in file to text

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const fs = require('fs');
+
 function RecordButton(props) {
   return (
     <button
@@ -11,13 +15,49 @@ function RecordButton(props) {
   )
 }
 export default class App extends React.Component {
-  handle_click() {
-    console.log("BUTTON CLICKED")
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null,
+    }
+  }
+  speech_to_text() {
+    console.log("-> Requsting transcription from cloud.google.com");
+    const client = new speech.SpeechClient();
+    const fileName = './resources/u.flac';
+    const file = fs.readFileSync(fileName);
+    const audioBytes = file.toString('base64');
+    const audio = {
+      content: audioBytes,
+    };
+    const config = {
+      encoding: 'FLAC',
+      sampleRateHertz: 44100,
+      languageCode: 'pl',
+    };
+    const request = {
+      audio: audio,
+      config: config,
+    };
+
+    client
+      .recognize(request)
+      .then(data => {
+        const response = data[0];
+        const transcription = response.results
+          .map(result => result.alternatives[0].transcript)
+          .join('\n');
+        this.setState({text: transcription});
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
   }
   render() {
     return (<div>
       <h2>Speech to Text Conversion for Patients</h2>
-      <RecordButton onClick={() => this.handle_click()} />
+      <RecordButton onClick={() => this.speech_to_text()} />
+      <p>{this.state.text}</p>
     </div>);
   }
 }
